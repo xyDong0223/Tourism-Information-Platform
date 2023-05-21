fcf2cd3950aba05e8f40bfa727b7fe97ef9a9c5a
xyDong0223 <dongxinyu23@gmail.com>
2023-05-22 02:51:15 +0800

    add sckscanner service

--- Broker/app.py
+++ Broker/app.py
@@ -1,39 +1,54 @@
 import math
 
-from flask import Flask, jsonify, request,render_template
+from flask import Flask, jsonify, request, render_template
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 import socketio
+
 app = Flask(__name__)
 CORS(app)
-socket = SocketIO(app,cors_allowed_origins='http://127.0.0.1:5000',async_mode='threading', transport='xhr-polling')
+socket = SocketIO(app, cors_allowed_origins='http://127.0.0.1:5000', async_mode='threading', transport='xhr-polling')
 
 # Kiwi
 sio_kiwi = socketio.Client()
 sio_kiwi.connect('http://localhost:5001')
+
+
 @sio_kiwi.on('service_response')
 def service_response(quotation):
     print(f"Received quotation: {quotation}")
-    socket.emit('kiwi_update',quotation)
-    sio_booking.disconnect()
+    socket.emit('kiwi_update', quotation)
+    sio_kiwi.disconnect()
+
+# Skyscanner
+sio_skyscanner = socketio.Client()
+sio_skyscanner.connect('http://localhost:5003')
+
+@sio_skyscanner.on('service_response')
+def service_response(quotation):
+    print(f"Received quotation: {quotation}")
+    socket.emit('skyscanner_update', quotation)
+    sio_skyscanner.disconnect()
+
 
 # Booking
 sio_booking = socketio.Client()
 sio_booking.connect('http://localhost:5004')
+
+
 @sio_booking.on('service_response')
 def service_response(quotation):
     print(f"Received quotation: {quotation}")
-    socket.emit('booking_update',quotation)
+    socket.emit('booking_update', quotation)
     sio_booking.disconnect()
 
 
 @socket.on('get_input')
 def get_input(data):
     print(f"Received quotation: {data}")
-    sio_kiwi.emit('calculate_service',data)
-    sio_booking.emit('calculate_service',data)
-
-
+    sio_kiwi.emit('calculate_service', data)
+    sio_skyscanner.emit('calculate_service', data)
+    sio_booking.emit('calculate_service', data)
 
 
 if __name__ == '__main__':
--- serviceSkyscanner/app.py
+++ serviceSkyscanner/app.py
@@ -1,16 +1,45 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask import Flask
+from flask_socketio import SocketIO
 from flask_cors import CORS
+import socketio
+
 app = Flask(__name__)
 CORS(app)
-socketio = SocketIO(app)
+socket = SocketIO(app, cors_allowed_origins='http://127.0.0.1:5005', async_mode='threading', transport='xhr-polling')
 
-# Define a route for the home page
-@app.route('/')
-def home():
-    return render_template('index.html')
+def calculate_quotation(start_date, end_date, city, travel_type):
+    # Base prices for each city (replace with real prices)
+    city_prices = {
+        'Rome': 2000,
+        'Paris': 2500,
+        'Dublin': 1500
+    }
+
+    # Percentage adjustments for each travel type
+    travel_type_adjustments = {
+        'all inclusive': 1.0,  # 100%
+        'semi inclusive': 0.9,  # 90%
+        'only "transport and hotel': 0.6   # 60%
+    }
+
+    base_price = city_prices.get(city, 0)
+    adjustment_factor = travel_type_adjustments.get(travel_type, 1)
+    price = base_price * adjustment_factor
+
+    description = f"{travel_type.capitalize()} trip to {city} from {start_date} to {end_date}."
+    return {"location": city, "price": price, "description": description}
 
 
+@socket.on('calculate_service')
+def get_input(data):
+    print(f"Received quotation request: {data}")
+    start_date = data['start_date']
+    end_date = data['end_date']
+    city = data['city']
+    travel_type = data['travel_type']
+    quotation = calculate_quotation(start_date, end_date, city, travel_type)
+    socket.emit('service_response', quotation)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5003)
+    socket.run(app, port=5003)
+
