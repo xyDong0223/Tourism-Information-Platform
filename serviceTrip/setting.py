4b14988411b238e63eabad54c41ae00884e44660
YimingZhao6324 <yimingzhao502@gmail.com>
2023-05-24 15:30:28 +0100

    Add Trip.com service

---
+++ serviceTrip/setting.py
@@ -0,0 +1,15 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    ENV = 'development'
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'Trip.sqlite')
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+
+class DevelopmentConfig(Config):
+    ENV = 'development'
+
+class ProducationConfig(Config):
+    ENV = "production"
+    DEBUG = False
