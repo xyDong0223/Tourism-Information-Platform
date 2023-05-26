ddf44762eb30500ddf8b96622fbd73a70438e3da
stevennanw <nan.wu1009@gmail.com>
2023-05-26 23:19:42 +0800

    fix connection issue

--- Broker/app.py
+++ Broker/app.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify, request, render_template
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 import socketio
+import time
 
 app = Flask(__name__)
 CORS(app)
@@ -11,49 +12,63 @@ socket = SocketIO(app, cors_allowed_origins='http://127.0.0.1:5000', async_mode=
 
 # Kiwi
 sio_kiwi = socketio.Client()
-sio_kiwi.connect('http://service-kiwi:5001')
-
-
-@sio_kiwi.on('service_response')
-def service_response(quotation):
-    print(f"Received quotation: {quotation}")
-    socket.emit('kiwi_update', quotation)
-
+connected = False
+while not connected:
+    try:
+        sio_kiwi.connect('http://service-kiwi:5001')
+        print("Socket established")
+        connected = True
+        @sio_kiwi.on('service_response')
+        def service_response(quotation):
+            print(f"Received quotation: {quotation}")
+            socket.emit('kiwi_update', quotation)
+    except Exception as ex:
+        time.sleep(1)
 
 # Trip
 sio_trip = socketio.Client()
-sio_trip.connect('http://service-trip:5002')
-
-
-@sio_trip.on('service_response')
-def service_response(quotation):
-    print(f"Received quotation: {quotation}")
-    socket.emit('trip_update', quotation)
-
+connected = False
+while not connected:
+    try:
+        sio_trip.connect('http://service-trip:5002')
+        print("Socket established")
+        connected = True
+        @sio_trip.on('service_response')
+        def service_response(quotation):
+            print(f"Received quotation: {quotation}")
+            socket.emit('trip_update', quotation)
+    except Exception as ex:
+        time.sleep(1)
 
 # Skyscanner
 sio_skyscanner = socketio.Client()
-sio_skyscanner.connect('http://service-skyscanner:5003')
-
-# Listen for the 'service_response' from the service
-@sio_skyscanner.on('service_response')
-def service_response(quotation):
-    print(f"Received quotation: {quotation}")
-    socket.emit('skyscanner_update', quotation)  # send quotation to client
-
+connected = False
+while not connected:
+    try:
+        sio_skyscanner.connect('http://service-skyscanner:5003')
+        print("Socket established")
+        connected = True
+        @sio_skyscanner.on('service_response')
+        def service_response(quotation):
+            print(f"Received quotation: {quotation}")
+            socket.emit('skyscanner_update', quotation)  # send quotation to client
+    except Exception as ex:
+        time.sleep(1)
 
 # Booking
 sio_booking = socketio.Client()
-sio_booking.connect('http://service-booking:5004')
-
-
-# Define a function to handle the response from the Auldfella's Quotation Service
-
-@sio_booking.on('service_response')
-def service_response(quotation):
-    print(f"Received quotation: {quotation}")
-    socket.emit('booking_update', quotation)
-
+connected = False
+while not connected:
+    try:
+        sio_booking.connect('http://service-booking:5004')
+        print("Socket established")
+        connected = True
+        @sio_booking.on('service_response')
+        def service_response(quotation):
+            print(f"Received quotation: {quotation}")
+            socket.emit('booking_update', quotation)
+    except Exception as ex:
+        time.sleep(1)
 
 @socket.on('get_input')
 def get_input(data):
